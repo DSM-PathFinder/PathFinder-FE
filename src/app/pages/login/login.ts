f01749220d0c5092cf08569f6dcdf45d11408cb6
서지유 <jihoseo2008@gmail.com>
2026-05-27 09:23:02 +0900

    feat :: login components

---
+++ src/app/pages/login/login.ts
@@ -0,0 +1,17 @@
+import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { LucideAngularModule } from 'lucide-angular';
+
+@Component({
+  selector: 'app-login',
+  standalone: true,
+  imports: [LucideAngularModule],
+  templateUrl: './login.html',
+})
+export class LoginComponent {
+  constructor(private router: Router) {}
+
+  handleLogin() {
+    this.router.navigate(['/onboarding']);
+  }
+}
