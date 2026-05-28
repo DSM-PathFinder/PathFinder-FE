858ac5dabcd5319ba956f45b03e4377a871201ef
서지유 <jihoseo2008@gmail.com>
2026-05-28 11:51:27 +0900

    feat :: layout component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,14 +2,18 @@ import { Routes } from '@angular/router';
 import { LoginComponent } from './pages/login/login';
 import { GeneratingComponent } from './pages/generating/generating';
 import { OnboardingComponent } from './pages/onboarding/onboarding';
+import { DashboardComponent } from './pages/dashboard/dashboard';
+import { LayoutComponent } from './components/layout/layout';
 
 export const routes: Routes = [
   {
     path: '',
+    component: LayoutComponent,
     redirectTo: 'login',
     pathMatch: 'full',
   },
   { path: 'login', component: LoginComponent },
   { path: 'onboarding', component: OnboardingComponent },
   { path: 'generating', component: GeneratingComponent },
+  { path: 'dashboard', component: DashboardComponent },
 ];
